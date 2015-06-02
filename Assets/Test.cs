ee3dc4e0831bde068cd3b2ebcb73e704a0858bad
Charlotte Pierce <charlotte@malformed-bits.com>
2015-06-02 22:35:13 +1000

    Updated test script.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,46 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Pitch;
 
 public class Test : MonoBehaviour {
-	PitchTracker _pitchTracker;
-	AudioClip _micInput;
-	float[] _samples;
-
-	void Start() {
-		int minFreq, maxFreq;
-
-		foreach (string device in Microphone.devices) {
-			Debug.Log("Name: " + device);
-			Microphone.GetDeviceCaps(device, out minFreq, out maxFreq);
-			Debug.Log("- " + minFreq + " -> " + maxFreq);
+	void Update () {
+		if (UnityPitchControl.Input.InputManager.GetKey(KeyCode.A)) {
+			Debug.Log("'a' down");
+		}
+		if (UnityPitchControl.Input.InputManager.GetKey(KeyCode.B)) {
+			Debug.Log("'b' down");
+		}
+		if (UnityPitchControl.Input.InputManager.GetKey(KeyCode.C)) {
+			Debug.Log("'c' down");
+		}
+		if (UnityPitchControl.Input.InputManager.GetKey(KeyCode.D)) {
+			Debug.Log("'d' down");
 		}
 
-		Microphone.GetDeviceCaps("Built-In Microphone", out minFreq, out maxFreq);
-		_micInput = Microphone.Start("Built-In Microphone", true, 1, maxFreq);
-
-		_samples = new float[_micInput.samples * _micInput.channels];
-		Debug.Log(_micInput.samples + " samples (" + _micInput.channels + " channel)");
-
-		_pitchTracker = new PitchTracker();
-		_pitchTracker.SampleRate = _micInput.samples;
-		_pitchTracker.PitchDetected += new PitchTracker.PitchDetectedHandler(PitchDetectedListener);
-	}
-
-	void Update() {
-		_micInput.GetData(_samples, 0);
-		Debug.Log("Samples length: " + _samples.Length);
-
-		float sum = 0;
-		foreach (float s in _samples) sum += s;
-		float avg = sum / _samples.Length;
-
-		Debug.Log("Samples avg: " + avg.ToString("0.0000000000"));
-
-		_pitchTracker.ProcessBuffer(_samples);
-	}
-
-	private void PitchDetectedListener(PitchTracker sender, PitchTracker.PitchRecord pitchRecord) {
-		Debug.Log("Pitch detected (" + pitchRecord.Pitch + ")");
+//		if (UnityPitchControl.Input.InputManager.GetKeyDown(KeyCode.A)) {
+//			Debug.Log("'a' down");
+//		}
+//		if (UnityPitchControl.Input.InputManager.GetKeyDown(KeyCode.B)) {
+//			Debug.Log("'b' down");
+//		}
+//		if (UnityPitchControl.Input.InputManager.GetKeyDown(KeyCode.C)) {
+//			Debug.Log("'c' down");
+//		}
+//		if (UnityPitchControl.Input.InputManager.GetKeyDown(KeyCode.D)) {
+//			Debug.Log("'d' down");
+//		}
+//		
+//		if (UnityPitchControl.Input.InputManager.GetKeyUp(KeyCode.A)) {
+//			Debug.Log("'a' up");
+//		}
+//		if (UnityPitchControl.Input.InputManager.GetKeyUp(KeyCode.B)) {
+//			Debug.Log("'b' up");
+//		}
+//		if (UnityPitchControl.Input.InputManager.GetKeyUp(KeyCode.C)) {
+//			Debug.Log("'c' up");
+//		}
+//		if (UnityPitchControl.Input.InputManager.GetKeyUp(KeyCode.D)) {
+//			Debug.Log("'d' up");
+//		}
 	}
 }
